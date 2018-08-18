2100684af413bee4b24731f42a6cefab34a88725
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-08-18 09:42:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // Code your solution in this file!
 // Code your solution in this file!
 const companyName = 'Scuber';
-companyName = 'specialCompany';
 let mostProfitableNeighborhood = 'Chelsea';
 let companyCeo = 'Susan Smith';
-companyCeo = 'Lauren Hart';
